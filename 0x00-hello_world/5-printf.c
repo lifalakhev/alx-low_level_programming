98d8673186a18ac58285593c5e713d5be73f308a
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-07-13 14:45:36 -0700

    5-printf.c updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,8 @@
  * main - A program that print a new line using printf function
  * Return: 0 (Successfu)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
 		return (0);
 }
-
