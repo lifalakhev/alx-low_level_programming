4aafcd76630e8d4c278497a6a256d285ceab9c31
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-07-13 13:39:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints a line with puts function
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
