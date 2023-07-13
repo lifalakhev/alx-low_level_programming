21c1e4d26e25d5c9f75fbb2b124a58545fe39d34
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-07-13 16:08:28 -0700

    6-size.c bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
- /**
-  * main - A program that print a new line using printf function
-  * Return: 0 (Successful)
-  */
+/**
+ * main - A prorgram that print a new line using printf function
+ * Return 0 (successful)
+ */
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -10,5 +10,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-return (0);
+	return (0);
 }
