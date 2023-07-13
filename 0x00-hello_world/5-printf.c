2954320a36a9503f1eebc19371331ce39d389d96
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-07-13 14:57:44 -0700

    5-printf.c spaces removed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that print a new line using printf function
- * Return: 0 (Successfu)
+ * Return: 0 (Successful)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
