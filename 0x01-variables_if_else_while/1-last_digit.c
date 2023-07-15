4e2adf04b09193f5faaa729d705f84f537f23b80
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-07-15 03:23:44 -0700

    new line added

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (lastd > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, lastd);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	}
 	else if (lastd == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, lastd);
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, lastd);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	}
 	return (0);
 }
