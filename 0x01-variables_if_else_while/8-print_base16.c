762b99bfc202848cc259cf6bb04bd865a5a1c1e1
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-07-14 20:42:45 -0700

    gcc error

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,17 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main()
+int main(void)
 {
 	int i;
-	        for (i = 0; i < 10; i++) 
-		{
-			        putchar('0' + i);				    }
-		    for (i = 0; i < 6; i++)
-		    {
-			            putchar('a' + i);
-		    }			    putchar('\n')
-			    return 0;
-}
 
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
