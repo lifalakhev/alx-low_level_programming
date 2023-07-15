ea89ae1bdd8533497da02841c756aa4ecd8365b0
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-07-15 08:58:59 -0700

    102-print_comb5.c fixed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,28 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digits numbeers
+ * main - prints two digit numbers
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i, j;
-	int a, b, c, d;
 
-	for (i = '0'; i <= 99; i++)
+	for (i = 0; i < 99; i++)
 	{
-		a = i / 10;
-		b = i % 10;
-
-		for (j = '0'; j <= 99; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			c = j / 10;
-			d = j % 10;
-			if (a < c || (a == c && b < d))
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
+				putchar(',');
 				putchar(' ');
-				putchar(c + '0');
-				putchar(d + '0');
-				if (a != '9' && b != '8')
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}
