9d43d34ebd9adf4c7166728786c140624c10e3f0
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-07-19 06:42:55 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * jack_bauer - prints the munites of a day
@@ -17,7 +17,7 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-					if (a >= 2; && b >= 4)
+					if (a >= 2 && b >= 4)
 						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
