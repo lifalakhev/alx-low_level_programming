820188edc7cf41cd0fad3956268f33dac41f8db6
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-07-24 09:08:33 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pits - prints a string, followed by a new line, to stdout
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
 void _puts(char *str)
