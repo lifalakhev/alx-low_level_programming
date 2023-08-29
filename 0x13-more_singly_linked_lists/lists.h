98d7d8422cdd8291362d3b1a84ded0529618f8ce
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-08-29 01:54:17 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,7 +16,7 @@ typedef struct listint_s
 {
     int n;
     struct listint_s *next;
-}
+}listint_t;
 
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
