37f9e579636be86f96862dfe2ff2d02905868335
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-10-30 06:05:25 -0700

    updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdint.h>
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error_and_exit(int exit_code, const char *error_message);
+void print_hex(uint8_t *buffer, size_t size);
+void print_elf_header(uint8_t *buffer);
 
 #endif
