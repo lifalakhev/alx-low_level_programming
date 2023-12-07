d03e1ca8fa4c5c1761edce20d18d447e28b64697
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-12-07 14:37:52 -0800

    updating 102-result

---
+++ 0x17-doubly_linked_lists/102-result.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+largest_palindone = 0
+
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        if str(product) == str(product)[::-1] and product > largest_palindrone:
+            largest_palindrone = product
+
+# Save the result in 102-result
+with open("102-result", "w") as file:
+    file.write(str(largest_palindrone))
