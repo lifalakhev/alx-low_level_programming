9c488ff49b40cb0c415f8cb5c08c264393c20a48
LIFALAKHE MASHABA <lifalakhe.lv@gmail.com>
2023-12-11 13:48:20 -0800

    operations

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+  
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: addition.
+ */
+int add(int a, int b)
+{
+        return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: subtraction.
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * mul - multiples two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: multiplication.
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: division.
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+
+/**
+ * mod - mod of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: mod.
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
